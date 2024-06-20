36133a8125ff3e4fbe5dec4cb525650892318744
kasashiki3 <naota383838@gmail.com>
2024-06-20 10:07:21 +0900

    tesuto

--- hidtaiko.py
+++ hidtaiko.py
@@ -1,42 +1,25 @@
 import streamlit as st
-import serial
-import time
+import usb.core
+import usb.backend.libusb1
 
-# Arduinoのシリアルポートを指定します
-SERIAL_PORT = 'COM3'  # 実際のポートに置き換えてください
-BAUD_RATE = 9600
+# USB Vendor IDとProduct IDを設定します
+VENDOR_ID = 0x0f0d  # ここにデバイスのVendor IDを入力してください
+PRODUCT_ID = 0x0092  # ここにデバイスのProduct IDを入力してください
 
-@st.cache_resource
-def get_serial_connection():
-    return serial.Serial(SERIAL_PORT, BAUD_RATE)
+# USBデバイスに接続する関数
+def connect_to_usb_device():
+    # Vendor IDとProduct IDを指定してUSBデバイスを検索します
+    dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+    
+    if dev is None:
+        st.write("デバイスが見つかりませんでした")
+    else:
+        st.write("デバイスが見つかりました")
+        # デバイスに対して操作を行うコードをここに追加します
 
-def read_from_arduino(serial_connection):
-    if serial_connection.in_waiting > 0:
-        return serial_connection.readline().decode('utf-8').strip()
-    return None
+# Streamlitアプリケーションの設定
+st.title('WebUSBを使用するアプリケーション')
 
-st.title('Arduino Serial Communication')
-
-# シリアル接続を取得
-ser = get_serial_connection()
-
-# Arduinoに送信するメッセージを入力
-message = st.text_input('Message to Arduino', '')
-
-if st.button('Send'):
-    if message:
-        ser.write(message.encode('utf-8'))
-        st.write('Message sent to Arduino:', message)
-
-# Arduinoからのメッセージを表示
-st.write('Message from Arduino:')
-output = read_from_arduino(ser)
-if output:
-    st.write(output)
-else:
-    st.write('No new messages.')
-
-# シリアル接続を閉じる
-if st.button('Close Connection'):
-    ser.close()
-    st.write('Serial connection closed.')
+# デバイスに接続するボタンを追加します
+if st.button('デバイスに接続'):
+    connect_to_usb_device()
